4a5fde88509cd177136cc699d62a26863f1b79ba
enjam rishikesh reddy <97114842+rishi63033@users.noreply.github.com>
2025-12-09 14:28:35 +0530

    Use environment variable for server port

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const db = require("./db/db")
 const authRoute = require("./route/auth")
 const dataRoutes = require("./route/data");
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 db();
@@ -14,4 +16,8 @@ app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
